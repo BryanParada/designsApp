497df54defd65d71f1b52708a69d2510f551533c
Bryan Parada <bryan.parada.cea@gmail.com>
2022-12-21 15:15:27 -0300

    F144 - basic design pt3

--- lib/screens/basic_design.dart
+++ lib/screens/basic_design.dart
@@ -14,7 +14,19 @@ class BasicDesignScreen extends StatelessWidget {
 
 
           //TItulo
-          Title()
+          Title(),
+
+
+          //button section
+          ButtonSection(),
+
+          //Descripcion
+          Container(
+            margin: EdgeInsets.symmetric( horizontal: 20, vertical: 10),
+            child: Text('Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem ')
+            )
+
+
         ],
       ),
     );
@@ -44,12 +56,60 @@ class Title extends StatelessWidget {
           ),
 
           //Expanded(child: Container( color: Colors.red, height: 10)), //para ver la separacion
-          Expanded(child: Container( color: Colors.red, height: 10)),
+          Expanded(child: Container( )),
 
           Icon( Icons.star, color: Colors.red),
           Text('41')
         ],
       ),
+    );
+  }
+}
+
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 60, vertical:40),
+            child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+            
+              CustomButton(icon: Icons.call, text: 'Call',),
+              CustomButton(icon: Icons.navigation_rounded, text: 'Route',),
+              CustomButton(icon: Icons.share, text: 'Share'),
+              
+      
+            ],
+          ), 
+        );
+    }
+}
+
+class CustomButton extends StatelessWidget {
+
+  final IconData icon;
+  final String text;
+
+  const CustomButton({
+    Key? key,
+    required this.icon,
+    required this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon( icon, color: Colors.blue, size:30),
+        Text(text, style: TextStyle(color: Colors.blue),)
+      ],
     );
   }
 }
