1924c94311ae024c69a5ca0143f919d1271c5e8f
Bryan Parada <bryan.parada.cea@gmail.com>
2022-12-22 14:11:10 -0300

    F152 - composite design - Blur

--- lib/widgets/card_table.dart
+++ lib/widgets/card_table.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class CardTable extends StatelessWidget {
@@ -6,7 +8,7 @@ class CardTable extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Table(
-      children: [
+      children: const [
         TableRow(
           children:[
             _SingleCard(icon: Icons.border_all, color: Colors.blue, text: 'General'),
@@ -65,23 +67,32 @@ class _SingleCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(15),
-      height: 180,
-      decoration: BoxDecoration(
-        color: Color.fromRGBO(62, 66, 107, 0.7),
-        borderRadius: BorderRadius.circular(20)
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          CircleAvatar(
-            backgroundColor: this.color,
-            child: Icon(this.icon),
-            radius: 30,
-          ),
-          SizedBox(height: 10,),
-          Text(this.text, style: TextStyle(color: this.color, fontSize: 18))
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: BackdropFilter(
+          filter: ImageFilter.blur( sigmaX: 5, sigmaY: 5),
+          child: Container(
             
-        ],
+            height: 180,
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(62, 66, 107, 0.7),
+              borderRadius: BorderRadius.circular(20)
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  backgroundColor: this.color,
+                  child: Icon(this.icon, size: 35, color: Colors.white),
+                  radius: 30,
+                ),
+                SizedBox(height: 10,),
+                Text(this.text, style: TextStyle(color: this.color, fontSize: 18))
+                  
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
