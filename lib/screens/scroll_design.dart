5ffcc5685de55fc81c971c4120856be1cf139232
Bryan Parada <bryan.parada.cea@gmail.com>
2022-12-21 22:18:16 -0300

    F147 - scroll design pt3

--- lib/screens/scroll_design.dart
+++ lib/screens/scroll_design.dart
@@ -1,20 +1,51 @@
 import 'package:flutter/material.dart';
 
 class ScrollScreen extends StatelessWidget {
-   
-  const ScrollScreen({Key? key}) : super(key: key);
+ 
+  final boxDecoration = BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            stops: [0.5,0.5],
+            colors: [
+              Color(0xff5EE8C5),
+              Color(0xff30BAD6)
+            ]
+          )
+        );
   
   @override
   Widget build(BuildContext context) {
+ 
     return  Scaffold(
-      body: Stack(
-        children: [
-          //background image
-          Background(),
-
-          MainContent()
-        ],
+      //backgroundColor: Color(0xff30BAD6),
+      body: Container(
+        decoration: boxDecoration,
+        child: PageView(
+          physics: BouncingScrollPhysics(),
+          scrollDirection: Axis.vertical,
+          children: [
+            Page1(),
+            Page2(), 
+          ],
+        ),
       )
+    );
+  }
+}
+
+class Page1 extends StatelessWidget {
+  const Page1({super.key}); 
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        //background image
+        Background(),
+
+        MainContent()
+      ],
     );
   }
 }
@@ -59,6 +90,30 @@ class Background extends StatelessWidget {
       height: double.infinity,
       alignment: Alignment.topCenter,
       child: Image(image: AssetImage('assets/scroll-1.png'))
+    );
+  }
+}
+
+class Page2 extends StatelessWidget {
+  const Page2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color(0xff30BAD6),
+      child: Center(
+        child: TextButton(
+          onPressed: () {},
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 50),
+            child: Text('Bienvenido', style: TextStyle(color: Colors.white, fontSize: 50)),
+          ),
+          style: TextButton.styleFrom(
+            backgroundColor: Color(0xff0098FA),
+            shape: StadiumBorder()
+          ),
+        ),
+      ),
     );
   }
 }
